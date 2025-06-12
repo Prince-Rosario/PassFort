eda0d84a973065414321736ea7c8bb0315a8e23f
Prince Rosario <princerosario2003@gmail.com>
2025-06-13 04:32:17 +0700

    chore: rename PostgreSQL container and adjust Dockerfile context path; update Tailwind CSS plugin reference and service worker revision

--- passfort-web/dev-dist/sw.js
+++ passfort-web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ht0danhl8ug"
+    "revision": "0.954d9rq7pgs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
