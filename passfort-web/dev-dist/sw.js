efb3fb2c1fcdb87174154d1a93c1c36649dfed28
Prince Rosario <princerosario2003@gmail.com>
2025-06-27 20:17:23 +0700

    chore: update database connection string to use localhost for local development; modify service worker to handle skip waiting event

--- passfort-web/dev-dist/sw.js
+++ passfort-web/dev-dist/sw.js
@@ -67,10 +67,13 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
+define(['./workbox-a896ab28'], (function (workbox) { 'use strict';
 
-  self.skipWaiting();
-  workbox.clientsClaim();
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
 
   /**
    * The precacheAndRoute() method efficiently caches and responds to
@@ -82,7 +85,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f0ens24mb24"
+    "revision": "0.d9qq0hk4lk4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
